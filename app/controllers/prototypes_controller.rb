ef106484ad94535ddff332f53791ceb78df2e932
ryosukegym <73817040+ryosukegym@users.noreply.github.com>
2020-12-10 14:56:35 +0900

    コントローラー修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -93,6 +93,6 @@ class PrototypesController < ApplicationController
 
   private
   def prototype_params
-    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.user.id)
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
   end
 end
