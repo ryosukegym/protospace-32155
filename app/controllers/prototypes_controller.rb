8045ce8b7c899362a1b641b0ced2da5d397cb954
ryosukegym <73817040+ryosukegym@users.noreply.github.com>
2020-12-10 12:09:23 +0900

    追加実装

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,8 +1,9 @@
 class PrototypesController < ApplicationController
   before_action  :authenticate_user!, except: [:index, :show]
-  # before_action :move_to_index, except: [:index ,:show]
+  # before_action :contributor_confirmation, only: [:edit, :update, :destroy]
+
   # before_action :move_to_new, except:[:destroy, :update, :edit]
-  before_action :move_to_index, only:[:edit, :show]
+  # before_action :move_to_index, only:[:edit, :show]
   def index
     @prototypes = Prototype.includes(:user)
   end
@@ -34,8 +35,25 @@ class PrototypesController < ApplicationController
   end
 
   def edit
-    @prototype = Prototype.find(params[:id])
+      @prototype = Prototype.find(params[:id])
+    if user_signed_in? 
+      if current_user.id!=@prototype.user.id
+        redirect_to root_path
+      end
+    else
+      render :index
+    end
   end
+      
+    
+    
+    
+    # current_users.id＝＝
+    #   redirect_to root_path
+    # else
+    #   @prototype = Prototype.find(params[:id])
+    #   render :index
+    # end
 
   def update
     prototype = Prototype.find(params[:id])
@@ -53,15 +71,6 @@ class PrototypesController < ApplicationController
     prototype.destroy
     redirect_to root_path
   end
-
-
-  def move_to_index
-    @prototype = Prototype.find(params[:id])
-    unless current_user.id == @prototype.user_id
-      redirect_to root_path
-    end
-  end
-
   # def move_to_new
   #   if user_signed_in? && current_user.id == @prototype.user_id
   #     redirect_to root_path
@@ -70,11 +79,20 @@ class PrototypesController < ApplicationController
   #   end
   # end
 
+    # def contributor_confirmation
+  #   redirect_to root_path unless current_user == @prototype.user
+  # end
+# end
+
+  # def move_to_index
+  #   unless user_signed_in? && current_users.id == @prototype.user.id
+  #     redirect_to root_path
+  #   end
+  # end
+
+
   private
   def prototype_params
-    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
-  end
-  def user_params
-    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.id)
+    params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id: current_user.user.id)
   end
 end
